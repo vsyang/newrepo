4770df7f27e14c8fec9a94766cbe4dee2cdb879e
vsyang <yang.vanessa85@gmail.com>
2025-10-27 20:28:14 -0600

    added changes

--- server.js
+++ server.js
@@ -16,12 +16,16 @@ const static = require("./routes/static")
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout")
+app.set("layout", "layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+// Index route
+app.get("/", function(req, res){
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information

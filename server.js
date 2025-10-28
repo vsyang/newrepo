198367e6278b5464fe6d1e1bfa396c1db869f3e3
vsyang <yang.vanessa85@gmail.com>
2025-10-27 20:48:05 -0600

    latest changes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const static = require("./routes/static")
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "layouts/layout")
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes

6d33e23b613285b18dd1a845a9f2a8dc4f823579
vsyang <yang.vanessa85@gmail.com>
2025-11-10 22:27:37 -0700

    found the error

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -4,9 +4,6 @@ const baseController = {}
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
     res.render("index", { title: "Home", nav })
-    console.log('baseController dir:', __dirname);
-    console.log('utilities path:', require.resolve("../utilities/index.js"));
-
 }
 
 module.exports = baseController

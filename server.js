f879b6c5a353be914225ab55173a85f1acb74227
vsyang <yang.vanessa85@gmail.com>
2025-11-11 15:47:13 -0700

    error handling messages

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities")
 
 /* ***********************
  * View Engine and Templates
@@ -25,9 +26,20 @@ app.set("layout", "./layouts/layout")
  *************************/
 app.use(static)
 // Index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
+// Account routes
+//app.use("/account", require("./routes/accountRoute"))
+
+/* ***********************
+* File Not Found Route - must be last route in list
+* Place after all routes
+* Unit 3, Basic Error Handling Activity
+*************************/
+app.use(async (req, res, next) => {
+  next({ status: 404, message: `Sorry, we appear to have lost that page.` })
+})
 
 /* ***********************
 * Express Error Handler
@@ -36,9 +48,10 @@ app.use("/inv", inventoryRoute)
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   })
 })

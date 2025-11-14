1aded3aced89ba88876ca14bce1d22289605b538
vsyang <yang.vanessa85@gmail.com>
2025-11-13 19:46:27 -0700

    fixed alerts to pass wave

--- utilities/index.js
+++ utilities/index.js
@@ -36,10 +36,10 @@ Util.buildClassificationGrid = async function(data){
       grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
       + 'details"><img src="' + vehicle.inv_thumbnail 
-      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-      +' on CSE Motors" /></a>'
+      +'" alt="Figure of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+      +' on CSE Motors" ></a>'
       grid += '<div class="namePrice">'
-      grid += '<hr />'
+      grid += '<hr >'
       grid += '<h2>'
       grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
       + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 

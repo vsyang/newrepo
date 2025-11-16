5cc55198d10b9ac8a120873bb2e0f4dc52763fda
vsyang <yang.vanessa85@gmail.com>
2025-11-16 03:01:05 -0700

    started activity 2 for week 4

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -2,7 +2,8 @@ const utilities = require("../utilities/index.js")
 const baseController = {}
 
 baseController.buildHome = async function(req, res){
-    const nav = await utilities.getNav()
+  const nav = await utilities.getNav()
+    // req.flash("notice", "This is a flash message.")
     res.render("index", { title: "Home", nav })
 }
 

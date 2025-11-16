9c7b4501c5c919ee387dd975e444a299e8b1576b
vsyang <yang.vanessa85@gmail.com>
2025-11-16 16:57:24 -0700

    hide/show password working

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,12 @@
 const passBtn = document.querySelector("#passBtn");
 passBtn.addEventListener("click", function () {
-    
-})
+    const passInput = document.getElementById("account_password");
+    const type = passInput.getAttribute("type");
+    if (type === "password") {
+        passInput.setAttribute("type", "text");
+        passBtn.innerHTML = "Hide Password";
+    } else {
+        passInput.setAttribute("type", "password");
+        passBtn.innerHTML = "Show Password";
+    }
+});

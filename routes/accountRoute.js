146e27b4cf0cdabfa081813707bcb700fbce08b5
vsyang <yang.vanessa85@gmail.com>
2025-11-16 21:29:10 -0700

    almost finished with lesson activities

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,6 +5,7 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require("../utilities/account-validation")
 
 /* ******************************************
  * Deliver Login View
@@ -15,5 +16,16 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
  * Deliver Registration View
  *******************************************/
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+
+/* ******************************************
+ * Process Registration
+ *******************************************/
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router
--- server.js
+++ server.js
@@ -15,21 +15,22 @@ const inventoryRoute = require("./routes/inventoryRoute")
 const accountRoute = require("./routes/accountRoute")
 const utilities = require("./utilities")
 const session = require("express-session")
-const pool = require('./database/')
+const pool = require("./database/")
+const bodyParser = require("body-parser")
 
 /* ***********************
  * Middleware
  * ************************/
- app.use(session({
-  store: new (require('connect-pg-simple')(session))({
-    createTableIfMissing: true,
-    pool,
-  }),
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  name: 'sessionId',
- }))
+app.use(session({
+store: new (require('connect-pg-simple')(session))({
+  createTableIfMissing: true,
+  pool,
+}),
+secret: process.env.SESSION_SECRET,
+resave: true,
+saveUninitialized: true,
+name: 'sessionId',
+}))
 
 // Express Messages Middleware
 app.use(require('connect-flash')())
@@ -37,6 +38,10 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 }) 
+
+// Body Parser Middleware for parsing application/ x-www-form-urlencoded
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 /* ***********************
  * View Engine and Templates

11a6e803a42c496e0a61bc9656798931cf3db246
vsyang <yang.vanessa85@gmail.com>
2025-11-17 17:40:31 -0700

    team hash password done

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,9 @@
+/* ****************************************
+*  Account Controller
+* *************************************** */
 const utilities = require("../utilities")
 const accountModel = require("../models/account-model")
+const bcrypt = require("bcryptjs")
 
 /* ****************************************
 *  Deliver login view
@@ -36,11 +40,25 @@ async function registerAccount(req, res) {
         account_password,
     } = req.body
 
+    // Hash the password before storing
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    })
+  }
+
     const regResult = await accountModel.registerAccount(
         account_firstname,
         account_lastname,
         account_email,
-        account_password
+        hashedPassword
     )
 
     if (regResult) {
